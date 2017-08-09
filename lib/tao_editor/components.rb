bfe9cc9979a61ed8006cacac725203b548106708
farthinker <farthinker@gmail.com>
2017-08-09 16:54:23 +0800

    CHG: make this gem auto load

--- lib/tao_editor/components.rb
+++
@@ -1,4 +0,0 @@
-require 'tao_editor/components/toolbar_items_config'
-require 'tao_editor/components/editor_component'
-require 'tao_editor/components/toolbar_component'
-require 'tao_editor/components/toolbar_items'
--- lib/tao_editor/engine.rb
+++ lib/tao_editor/engine.rb
@@ -1,14 +1,20 @@
 require 'tao_on_rails'
 require 'tao_ui'
 require 'tao_form'
-require 'tao_editor/components'
+# require 'tao_editor/components'
 
 module TaoEditor
   class Engine < Rails::Engine
 
+    config.eager_load_paths += Dir["#{config.root}/lib"]
+
     config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     paths['app/views'] << 'lib/views'
+
+    ::ActiveSupport.on_load :tao_components do
+      load_tao_components TaoEditor::Engine.root
+    end
 
   end
 end
